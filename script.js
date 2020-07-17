60f8a5ee021409abe47240abb503ab61f671ab43
nishandms <nishandms06@gmail.com>
2020-07-17 14:51:44 +0530

    final

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+  var x = document.getElementById("id1").value;
+  var y = "14321";
+  if (x==y) {
+    window.open("movie.html")
+  } else {
+    alert("Poi ticket edukkeda naari")
+  } 
+} 
